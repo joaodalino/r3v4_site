90eca97833d21d7592714ce1f8df493a927f0787
SquareMan <joaodalino123@gmail.com>
2023-04-23 01:06:38 -0300

    e tudo puta

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mc = require('minecraft-protocol');
 const server = mc.createServer({
   'online-mode': true,
   encryption: true,
-  host: 'e8b8bdb1-610f-4831-a046-c950dff1c72d',
+  host: '66.11.123.132',
   port: 2001,
   version: '1.16.5'
 });
