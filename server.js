eec028313351ef78514dc5a7b5560ac20d88c10b
SquareMan <joaodalino123@gmail.com>
2023-04-23 01:19:34 -0300

    e tudo puta

--- server.js
+++ server.js
@@ -1,15 +1,53 @@
-const mc = require('minecraft-protocol');
 
-const server = mc.createServer({
+var mc = require('minecraft-protocol');
+
+var server = mc.createServer({
   'online-mode': true,
-  encryption: true,
-  host: 'e8b8bdb1-610f-4831-a046-c950dff1c72d',
-  port: 2001,
+  encryption: true,    
+  host: 'e8b8bdb1-610f-4831-a046-c950dff1c72d',   
+  port: 2001,     
   version: '1.16.5'
 });
 
-server.on('login', function(client) {
-  console.log(client.username + ' entrou no servidor!');
-});
+const mcData = require('minecraft-data')(server.version)
 
-console.log('Servidor Minecraft rodando na porta 2001.');
+server.on('login', function(client) {
+  
+  let loginPacket = mcData.loginPacket
+
+  client.write('login', {
+    entityId: client.id,
+    isHardcore: false,
+    gameMode: 0,
+    previousGameMode: 255,
+    worldNames: loginPacket.worldNames,
+    dimensionCodec: loginPacket.dimensionCodec,
+    dimension: loginPacket.dimension,
+    worldName: 'minecraft:overworld',
+    hashedSeed: [0, 0],
+    maxPlayers: server.maxPlayers,
+    viewDistance: 10,
+    reducedDebugInfo: false,
+    enableRespawnScreen: true,
+    isDebug: false,
+    isFlat: false
+  });
+
+  client.write('position', {
+    x: 0,
+    y: 1.62,
+    z: 0,
+    yaw: 0,
+    pitch: 0,
+    flags: 0x00
+  });
+  
+  var msg = {
+    translate: 'chat.type.announcement',
+    "with": [
+      'Server',
+      'Hello, world!'
+    ]
+  };
+  client.write("chat", { message: JSON.stringify(msg), position: 0, sender: '0' });
+});
